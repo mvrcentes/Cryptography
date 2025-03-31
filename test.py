dbe3dfef026635465aad814ad05df6f5ecf17436
mvrcentes <mvrcentes@hotmail.com>
2025-03-31 11:16:31 -0600

    Add: add Usopp challenge

---
+++ test.py
@@ -0,0 +1,4 @@
+from utils.usopp_cipher import decrypt
+
+# Llama la función decrypt con tu flag
+decrypt("a77742694e4807d31f3b3b308196d52d196808649d4e198d421572e915afc68dea4652dd68")
--- utils/extract_text_from_image.py
+++ utils/extract_text_from_image.py
@@ -21,7 +21,7 @@ def extraer_texto_metadata(imagen_path):
 # Uso del código para descifrar la imagen
 # Ejemplo de uso
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-image_path = os.path.join(base_dir, "recovered_data/extracted_images", "poneglyph_zoro.jpeg")
+image_path = os.path.join(base_dir, "recovered_data/extracted_images", "poneglyph_usopp.jpeg")
 student_id = input("Introduce tu carné para descifrar el mensaje: ")
 texto_cifrado = extraer_texto_metadata(image_path)
 decrypted_text = xor_cipher(texto_cifrado, student_id)
